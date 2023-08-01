6ae95eb5757097efa6ce6b472bf48a1e2b646574
Francisco Castillo <frankycstl@gmail.com>
2023-08-01 14:09:08 -0700

    init files configuration

---
+++ personal-web-vite/src/components/About.jsx
@@ -0,0 +1,8 @@
+export default function About () {
+    return (
+        <div className="aboutjsx">
+            <h1>Welcome!</h1>
+            <p>Hi, for now</p>
+        </div>
+    )
+}
---
+++ personal-web-vite/src/components/Nav/Nav.jsx
@@ -0,0 +1,16 @@
+import reactLogo from '../../../public/assets/react.svg'
+import './nav.css'
+
+export default function Nav () {
+    return (
+        <header>
+            <nav className='nav'>
+            
+            <div>
+                <a href="" target="_blank"><img src={reactLogo} className="logo" alt="React logo" /></a>
+            </div>
+            
+            </nav>
+        </header>
+    )
+}
